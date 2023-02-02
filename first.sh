9350065103e8bbe83f6d02ce607a96621bc6b4c5
Akhil0809 <47007823+Akhil0809@users.noreply.github.com>
2023-02-02 17:55:48 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source .env
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
@@ -6,6 +7,6 @@ sudo apt-get update
 sudo apt-get upgrade -y 
 sudo apt-get install docker docker-compose -y
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 310961312410.dkr.ecr.ap-south-1.amazonaws.com
-docker pull 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:latest
+docker pull 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:$Version
 docker images
-docker run -itd --name test-container 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:latest
+docker run -itd --name test-container 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:$Version
