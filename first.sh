6eefd34f8db08cf93498a2ef9d8912b9ff614e76
Akhil0809 <47007823+Akhil0809@users.noreply.github.com>
2023-02-07 18:38:14 +0530

    Update first.sh

--- first.sh
+++ first.sh
@@ -10,4 +10,6 @@ sudo apt-get install docker docker-compose -y
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 310961312410.dkr.ecr.ap-south-1.amazonaws.com
 docker pull 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:$Version
 docker images
-docker run -itd --name test-container-$containername 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:$Version --env-file=.env
+git init
+export $(cat .env | egrep -v "(^#.*|^$)" | xargs)
+docker run -itd --name test-container-$containername 310961312410.dkr.ecr.ap-south-1.amazonaws.com/test:$Version 
